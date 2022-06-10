31b5dbb87c6916ae45f81f84c48fa6c268fa2059
nikolay <veselovnd@gmail.com>
2022-06-10 21:55:24 +0300

    modifiers

--- JavarushMTProject/src/Modifiers.java
+++ JavarushMTProject/src/Modifiers.java
@@ -1,4 +1,25 @@
+import java.lang.reflect.Method;
 
 public class Modifiers {
+	public static void main(String[] args) {
+        int classModifiers = Modifiers.class.getModifiers();
+        System.out.println(isModifierSet(classModifiers, Modifier.PUBLIC));   //true
+        System.out.println(isModifierSet(classModifiers, Modifier.STATIC));   //false
 
+        int methodModifiers = getMainMethod().getModifiers();
+        System.out.println(isModifierSet(methodModifiers, Modifier.STATIC));      //true
+    }
+
+    public static boolean isModifierSet(int allModifiers, int specificModifier) {
+        return false;
+    }
+
+    private static Method getMainMethod() {
+        Method[] methods = Modifiers.class.getDeclaredMethods();
+        for (Method method : methods) {
+            if (method.getName().equalsIgnoreCase("main")) return method;
+        }
+
+        return null;
+    }
 }
