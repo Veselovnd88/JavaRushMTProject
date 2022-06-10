e843f306c9a1c60a731d85e835d4b04db2c04bad
nikolay <veselovnd@gmail.com>
2022-06-10 22:25:47 +0300

    модификаторы

--- JavarushMTProject/src/Modifiers.java
+++ JavarushMTProject/src/Modifiers.java
@@ -1,5 +1,12 @@
+import java.lang.reflect.Modifier;
 import java.lang.reflect.Method;
+/*Реализовать логику метода isModifierSet, который проверяет, 
+ * содержит ли переданный параметр allModifiers значение конкретного
+ *  модификатора specificModifier.
 
+P.S. Перед выполнением задания ознакомься с классом Modifier и 
+реализацией методов isPublic, isStatic и т.п.
+ * */
 public class Modifiers {
 	public static void main(String[] args) {
         int classModifiers = Modifiers.class.getModifiers();
@@ -11,7 +18,12 @@ public class Modifiers {
     }
 
     public static boolean isModifierSet(int allModifiers, int specificModifier) {
-        return false;
+    	//метод гетМодифаерс возвращает битовое значение модификаторов
+    	//а каждый из модификаторов обозначается также в двоичной системе
+    	//соответственно для того чтобы понять есть ли этот модификатор или нет применяем
+    	//побитовое AND-> если полученное значение > 0 значит такой модификатор есть
+    	//например паблик 0000 0001, Статик 0000 1000.
+        return (allModifiers&specificModifier)>0;
     }
 
     private static Method getMainMethod() {
