4861f9a3b307c32bfb245441895d1c2803524d2c
nikolay <veselovnd@gmail.com>
2022-06-11 15:09:45 +0300

    simplify

--- JavarushMTProject/src/Simplify.java
+++ JavarushMTProject/src/Simplify.java
@@ -1,9 +1,12 @@
 
 public class Simplify {
     public static boolean calculate(boolean a, boolean b, boolean c, boolean d) {
-        return (a && b && c && !d) || (!a && c) ||   (!b && c) || (c && d);
-        // 1 - если а,б и с -тру, а д фолс и наоборот   
-        //
+        return (a && b && c && !d) || //чтобы было тру с-тру, д фолс
+        		(!a && c) ||//чтобы было тру - с-тру - а фолс
+        		(!b && c) ||//чтобы было тру с-тру б фолс
+        		(c && d);//чтобы было тру - с тру д тру
+        //								 
+        // return c;
         
     }
 
