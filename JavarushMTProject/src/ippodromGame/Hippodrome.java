b05eb0137edb7a4145fa2c143104103a73ca8399
'Nikolay' <veselovnd@gmail.com>
2022-06-19 15:08:34 +0300

    game done

--- JavarushMTProject/src/ippodromGame/Hippodrome.java
+++ JavarushMTProject/src/ippodromGame/Hippodrome.java
@@ -5,11 +5,22 @@ import java.util.List;
 
 public class Hippodrome {
 	
+	public static Hippodrome game;
+	
 	private List<Horse> horses;
 	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// a) Создать список лошадей (horses).
+		List<Horse> horses = new ArrayList<>();
+		horses.add(new Horse("Vasya",3.0,0.0));
+		horses.add(new Horse("Petya",3.0,0.0));
+		horses.add(new Horse("Valera",3.0,0.0));
+		Hippodrome hp = new Hippodrome(horses);
+		Hippodrome.game = hp;
+		game.run();
+		game.printWinner();
+		
 
 	}
 	
@@ -17,6 +28,52 @@ public class Hippodrome {
 	public Hippodrome(List<Horse> horses) {
 		
 		this.horses = horses;
+	}
+	public void run() {
+		for (int i = 0; i < 100; i++) {
+			move();
+			print();
+			try {
+				Thread.sleep(200);//чтобы цикл не отработал за долю секунды
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+	}
+	
+	public Horse getWinner() {
+		double max=0;
+		Horse winner =null;
+		for(Horse h: horses) {
+			if(h.getDistance()>=max) {
+				max = h.getDistance();
+				winner = h;
+			}
+		}
+		return winner;
+	}
+	
+	public void printWinner() {
+		System.out.println("Winner is "+getWinner().getName()+"!");
+	}
+	
+	public void move() {
+		for(Horse h: horses) {
+			h.move();
+		}
+		
+	}
+	public void print() {
+		for(Horse h: horses) {
+			h.print();
+
+		}
+		for(int i=0; i<10; i++) {
+				System.out.println();
+			}
+		
 	}
 
 
