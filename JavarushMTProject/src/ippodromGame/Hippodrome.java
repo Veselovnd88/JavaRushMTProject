a3a68533186d696b9650aa5e0e9b01b983389a1e
'Nikolay' <veselovnd@gmail.com>
2022-06-19 14:33:51 +0300

    hippodrome

---
+++ JavarushMTProject/src/ippodromGame/Hippodrome.java
@@ -0,0 +1,30 @@
+package ippodromGame;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hippodrome {
+	
+	private List<Horse> horses;
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+
+	public Hippodrome(List<Horse> horses) {
+		
+		this.horses = horses;
+	}
+
+
+	public List<Horse> getHorses() {
+		return horses;
+	}
+	
+	
+	
+
+}
---
+++ JavarushMTProject/src/ippodromGame/Horse.java
@@ -0,0 +1,20 @@
+package ippodromGame;
+
+public class Horse {
+	private String name;
+	private double speed;
+	private double distance;
+	
+	public Horse(String name, double speed, double distance) {
+		super();
+		this.name = name;
+		this.speed = speed;
+		this.distance = distance;
+	}
+	
+	
+	
+	
+	
+
+}
