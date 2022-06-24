7dc45be02a458e94d5eb829fe4507bde92ae8479
nikolay <veselovnd@gmail.com>
2022-06-24 22:16:07 +0300

    c

--- JavarushMTProject/src/AnonymClass/Solution.java
+++ JavarushMTProject/src/AnonymClass/Solution.java
@@ -8,6 +8,9 @@ import java.util.List;
 
 import AnonymClass.vo.Location;
 import AnonymClass.vo.NamedItem;
+import AnonymClass.vo.Server;
+import AnonymClass.vo.Subject;
+import AnonymClass.vo.Subscription;
 import AnonymClass.vo.User;
 
 /* 
@@ -34,6 +37,36 @@ public class Solution {
     }
     public List<Location> getLocations(){
     	return  new AbstractDbSelectExecutor<Location>() {
+
+			@Override
+			public String getQuery() {
+				
+				return "SELECT *FROM LOCATION";
+			}
+		}.execute();
+    }
+    public List<Server> getServers(){
+    	return  new AbstractDbSelectExecutor<Server>() {
+
+			@Override
+			public String getQuery() {
+				
+				return "SELECT *FROM SERVER";
+			}
+		}.execute();
+    }
+    public List<Subject> getSubjects(){
+    	return  new AbstractDbSelectExecutor<Subject>() {
+
+			@Override
+			public String getQuery() {
+				
+				return "SELECT *FROM LOCATION";
+			}
+		}.execute();
+    }
+    public List<Subscription> getSubscriptions(){
+    	return  new AbstractDbSelectExecutor<Subscription>() {
 
 			@Override
 			public String getQuery() {
