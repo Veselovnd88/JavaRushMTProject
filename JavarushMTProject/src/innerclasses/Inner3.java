8c9a7377987b47de1550042a9de8b1a79d393bad
Nikolay <veselovnd@gmail.com>
2022-06-24 16:02:28 +0300

    inners

--- JavarushMTProject/src/innerclasses/Inner3.java
+++ JavarushMTProject/src/innerclasses/Inner3.java
@@ -1,5 +1,52 @@
 package innerclasses;
 
+import java.util.List;
+import java.util.Map;
+
 public class Inner3 {
+	
+    private List<Task> tasks;
+    private List<String> names;
+
+    private DbDataProvider taskDataProvider = new TaskDataProvider();
+    private DbDataProvider nameDataProvider = new NameDataProvider();
+
+    public void refresh() {
+        Map taskCriteria = MockView.getFakeTaskCriteria();
+        taskDataProvider.refreshAllData(taskCriteria);
+
+        Map nameCriteria = MockView.getFakeNameCriteria();
+        nameDataProvider.refreshAllData(nameCriteria);
+    }
+
+    private interface DbDataProvider<T> {
+        void refreshAllData(Map criteria);
+    }
+
+    class Task {
+    }
+
+    public static void main(String[] args) {
+
+    }
+    
+    private class TaskDataProvider implements DbDataProvider<Task> {
+
+		@Override
+		public void refreshAllData(Map criteria) {
+			tasks = MockDB.getFakeTasks(criteria);
+			
+		}
+	
+    }
+    
+    private class NameDataProvider implements DbDataProvider<String>{
+
+		@Override
+		public void refreshAllData(Map criteria) {
+			names = MockDB.getFakeNames(criteria);			
+		}
+    	
+    }
 
 }
--- JavarushMTProject/src/innerclasses/MockDB.java
+++ JavarushMTProject/src/innerclasses/MockDB.java
@@ -1,5 +1,18 @@
 package innerclasses;
 
-public class MockDB {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
+public class MockDB {
+    private static final List<Inner3.Task> FAKE_TASKS = new ArrayList<>();
+    private static final List<String> FAKE_NAMES = new ArrayList<>();
+
+    public static List<Inner3.Task> getFakeTasks(Map criteria) {
+        return MockView.getFakeTaskCriteria().equals(criteria) ? FAKE_TASKS : null;
+    }
+
+    public static List<String> getFakeNames(Map criteria) {
+        return MockView.getFakeNameCriteria().equals(criteria) ? FAKE_NAMES : null;
+    }
 }
