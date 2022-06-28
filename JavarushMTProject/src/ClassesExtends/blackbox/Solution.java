a4788358195a65840312a72e1ef6e05b1e3926b6
Nikolay <veselovnd@gmail.com>
2022-06-28 13:44:26 +0300

    blackbox

--- JavarushMTProject/src/ClassesExtends/blackbox/Solution.java
+++ JavarushMTProject/src/ClassesExtends/blackbox/Solution.java
@@ -6,19 +6,54 @@ Black box
 
 public class Solution implements Action {
     public static int countActionObjects;
-
     private int param;
 
     private Action solutionAction = new Action() {
-    
     	
-        public void someAction() {
-        if (param>0) {
-        	
-        }
-            
-            
-        }
+    	private FirstClass fc;
+    	private SecondClass sc;
+    	
+		@Override
+		public void someAction() {
+			if (param >0) {
+				if(fc==null)
+					fc = new FirstClass() {			
+						@Override
+						public void someAction() {
+							super.someAction();//вызываем метод родительского класса - (это печатает строку class
+							Solution.this.someAction();//вызываем метод экземпляра
+						}
+						@Override
+						public Action getDependantAction() {
+							System.out.println(param);
+							param--;
+							return param > 0 ? Solution.this : this;
+							
+							// если параметр больше нуля, то возвращается экземпляр Solution.Action, если меньше - то метод Fc
+							//этот метод возвращает Действие - дальше сразу вызывается его метод СамЭкшн
+						}
+				
+				};
+				fc.getDependantAction().someAction();
+			}
+			else { 
+				if(sc==null) {
+					sc = new SecondClass() {
+						@Override
+						public  void someAction() {
+							sb.append(SecondClass.SPECIFIC_ACTION_FOR_ANONYMOUS_SECOND_CLASS_PARAM+param);
+							super.someAction();
+							
+						}
+					};
+					sc.someAction();
+				}
+				
+			}
+
+			
+		}
+    	
     };
 
 
@@ -50,9 +85,9 @@ public class Solution implements Action {
         solution.someAction();
         System.out.println("Count of created Action objects is " + countActionObjects);
 
-        solution = new Solution(-1);
-        solution.someAction();
-        System.out.println("Count of created Action objects is " + countActionObjects);
+      solution = new Solution(-1);
+      solution.someAction();
+      System.out.println("Count of created Action objects is " + countActionObjects);
     }
 }
 
