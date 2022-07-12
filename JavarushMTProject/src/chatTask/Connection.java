a424929bd0c3e3436fba98d0b707654f2aebcb36
Nikolay <veselovnd@gmail.com>
2022-07-12 14:03:42 +0300

    chat 5-6-7

--- JavarushMTProject/src/chatTask/Connection.java
+++ JavarushMTProject/src/chatTask/Connection.java
@@ -1,5 +1,60 @@
 package chatTask;
+/*Этот класс обертка над классом net.Socket
+ * будет сериализовать и десериализовать данные - объекты Message в сокет
+ * методы могут вызываться из разныъ потоков*/
 
-public class Connection {
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+
+public class Connection implements Closeable {
+	private final Socket socket;
+	private final ObjectOutputStream out;
+	private final ObjectInputStream in;
+
+	
+	public Connection(Socket socket) throws IOException {
+		this.socket = socket;
+		this.out = new ObjectOutputStream(socket.getOutputStream());//создали потоки  - исх
+		this.in = new ObjectInputStream(socket.getInputStream());//поток вх.
+	}
+	
+	public void send (Message message) throws IOException {
+		synchronized(out) {//синхронизация на объекте out
+			out.writeObject(message);}
+		
+	}
+	
+	public Message receive() throws ClassNotFoundException, IOException {
+		synchronized(in){//синхронизация на объекте in
+			Message message = (Message) in.readObject();//десериализация - кастуем к нашему объекту
+			return message;
+		}
+	}
+	
+	public SocketAddress getRemoteSocketAddress() {
+		return socket.getRemoteSocketAddress();
+	}
+	
+	
+	public void close() throws IOException {
+		socket.close();
+		out.close();
+		in.close();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
--- JavarushMTProject/src/chatTask/Server.java
+++ JavarushMTProject/src/chatTask/Server.java
@@ -1,8 +1,67 @@
 package chatTask;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/*Сервер поддерживает много соединений с разными клиентами
+ * Сервер создает сокетное соединение -> с цикле ждет пока кто нибудь не подключится, создает поток обработчик - Handler
+ * в котором будет происходить обмен сообщениями, ожидает следующее подключение
+ * */
 public class Server {
 	
+	private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();//мапа всех соединений, ключ - имя клиента, значение - объект коннекшн, потокобезопасный
+
 	public static void main(String[] args) {
+		ConsoleHelper.writeMessage("Введите номер порта для создания сокета");
+		int port = ConsoleHelper.readInt();
+		ServerSocket ss = null;
+		try {
+			ss = new ServerSocket(port);//создание серверного сокета на порте
+			ConsoleHelper.writeMessage("Сервер запущен");
+			while(true) {
+				Socket income = ss.accept();//сокет слушает пока не придет входящее соединение
+				Handler handler = new Handler(income);// когда пришло создается хэндлер и запускается
+				handler.start();//создали и запустили нить
+			}
+			
+			
+			
+		} catch (IOException e) {
+			try {
+				ss.close();
+			} catch (IOException e1) {
+				ConsoleHelper.writeMessage("Ошибка закрытия сокета");
+				e1.printStackTrace();
+			}
+		}
+		
+	}
+	public void sendBroadcastMessage(Message message) {
+		
+		connectionMap.forEach((x,y)-> {
+			try {
+				y.send(message);
+			} catch (IOException e) {
+				ConsoleHelper.writeMessage("Не удалось отправить сообщение");
+				e.printStackTrace();
+			}
+		});
+	}
+	
+	
+	
+	private static class Handler extends Thread{//пприватный статический класс
+		private Socket socket;
+		
+		public Handler(Socket socket) {
+			this.socket = socket;
+		}
+		
+		
+		
 		
 	}
 
