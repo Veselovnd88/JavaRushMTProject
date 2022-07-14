d6a0ed0ef19eddbef43c1aa5a5cd42af060211c7
Nikolay <veselovnd@gmail.com>
2022-07-14 16:50:08 +0300

    deadlocks

---
+++ JavarushMTProject/src/waitNotify/ConsumerTask.java
@@ -0,0 +1,23 @@
+package waitNotify;
+
+public class ConsumerTask implements Runnable {
+    private TransferObject transferObject;
+    protected volatile boolean stopped;
+
+    public ConsumerTask(TransferObject transferObject) {
+        this.transferObject = transferObject;
+        new Thread(this, "ConsumerTask").start();
+    }
+
+    public  void run() {
+
+        while (!stopped) {
+
+            transferObject.get();}
+
+    }
+
+    public void stop() {
+        stopped = true;
+    }
+}
---
+++ JavarushMTProject/src/waitNotify/Solution.java
@@ -0,0 +1,14 @@
+package waitNotify;
+
+
+public class Solution {
+    public static void main(String args[]) throws InterruptedException {
+        TransferObject transferObject = new TransferObject();
+        ProducerTask producerTask = new ProducerTask(transferObject);
+        ConsumerTask consumerTask = new ConsumerTask(transferObject);
+
+        Thread.sleep(50);
+        producerTask.stop();
+        consumerTask.stop();
+    }
+}
