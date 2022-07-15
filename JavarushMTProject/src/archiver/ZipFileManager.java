6ef7efe8c9211cc47b4302834fe5b410a8df18b0
Nikolay <veselovnd@gmail.com>
2022-07-15 21:40:53 +0300

    archive

--- JavarushMTProject/src/archiver/ZipFileManager.java
+++ JavarushMTProject/src/archiver/ZipFileManager.java
@@ -2,11 +2,11 @@ package archiver;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
-
 public class ZipFileManager {
     Path zipFile;//полный путь к архиву с которым будем работать
 
@@ -16,21 +16,40 @@ public class ZipFileManager {
 
 
     public void createZip(Path source) throws IOException {//пусть к файлу который мы будем архивировать
+        Path dir = zipFile.getParent();
+        if(Files.notExists(dir)){
+            Files.createDirectory(dir);
+        }
+
 
 
         try(ZipOutputStream zos= new ZipOutputStream(Files.newOutputStream(zipFile));//открыли потока для записи в архив
             )//открыли поток для чтения из файла
-        {  ZipEntry ze = new ZipEntry(source.getFileName().toString());//создали ентри для ввода в архив
-            zos.putNextEntry(ze);// посадили ентри в наш архив
-            try(InputStream is = Files.newInputStream(source);){
+        {if(Files.isRegularFile(source)){//если сорс - обычный файл, то просто его кладем
+            addNewZipEntry(zos,zipFile,source);
+        } else if (
+                Files.isDirectory(source)
+        ) {FileManager fm = new FileManager(source);
 
-                    byte[] buffer = new byte[1024];//массив байтов
-                    int len = 0; //сколько прочитали
-                    while((len = is.read(buffer))>0){//пока то что вычитал больше нуля
-                    zos.write(buffer,0,len);//пишем весь буффер
-                }
-                    zos.closeEntry();
-            }
+
+
+        }
+            zos.closeEntry();
+        }
+    }
+    private void addNewZipEntry(ZipOutputStream zipOutputStream, Path filePath, Path filename) throws IOException{
+        ZipEntry ze = new ZipEntry(filename.toString());
+        try(InputStream is = Files.newInputStream(filename.resolve(filePath))){
+            zipOutputStream.putNextEntry(ze);
+            copyData(is,zipOutputStream);
+            zipOutputStream.closeEntry();
+        }
+    }
+    private void copyData(InputStream is, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int len=0;
+        while ((len=is.read(buffer))>0){
+            out.write(buffer,0,len);
         }
     }
 
