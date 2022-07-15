ce36d03212ff0d57dbd04a7591e68fdb4dfc7e9b
Nikolay <veselovnd@gmail.com>
2022-07-15 16:01:03 +0300

    Arhiver 8

--- JavarushMTProject/src/archiver/Archiver.java
+++ JavarushMTProject/src/archiver/Archiver.java
@@ -1,6 +1,7 @@
 package archiver;
 
 import archiver.command.ExitCommand;
+import archiver.exception.WrongZipFileException;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -9,26 +10,41 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Archiver {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Введите полный путь к архиву");
 
-        try {
-            String path = br.readLine();
-            Path p = Paths.get(path);
-            ZipFileManager zfm = new ZipFileManager(p);
-            System.out.println("Введите полный путь к файлу который будем архивировать");
-            String pathFile = br.readLine();
-            Path f = Paths.get(pathFile);
-            zfm.createZip(f);
-            br.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
+        Operation op=null;
+        while(op!=Operation.EXIT){
+
+            try{
+            op=askOperation();
+            }
+            catch (IOException e){//WRONGZIPFILE EXCEPTION
+                System.out.println("Вы не выбрали файл архива или выбрали неверный файл.");
+            }
+            catch (Exception e){
+                System.out.println("Произошла ошибка. Проверьте введенные данные.");
+            }
+        }
         new ExitCommand().execute();
 
     }
+    public static Operation askOperation() throws IOException{
+        System.out.printf("Выберите операцию:\n" +
+                "%s - упаковать файлы в архив\n" +
+                "%s - добавить файл в архив\n" +
+                "%s - удалить файл из архива\n" +
+                "%s - распаковать архив\n" +
+                "%s - просмотреть содержимое архива\n" +
+                "%s - выход\n",Operation.CREATE.ordinal(),Operation.ADD.ordinal(),Operation.REMOVE.ordinal(),Operation.EXTRACT.ordinal(),
+                Operation.CONTENT.ordinal(),Operation.EXIT.ordinal());
+        int op = ConsoleHelper.readInt();
+        return Operation.values()[op];
+
+    }
+
 
 
 }
--- JavarushMTProject/src/archiver/command/ZipExtractCommand.java
+++ JavarushMTProject/src/archiver/command/ZipExtractCommand.java
@@ -1,5 +1,7 @@
 package archiver.command;
 
+import archiver.exception.WrongZipFileException;
+
 public class ZipExtractCommand extends ZipCommand{
     @Override
     public void execute() {
