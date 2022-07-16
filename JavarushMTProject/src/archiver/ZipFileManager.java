6256c35c88fa87b0158bc2fd4d0ea5ba280edd7c
nikolay <veselovnd@gmail.com>
2022-07-16 16:22:23 +0300

    10 Arhiver

--- JavarushMTProject/src/archiver/ZipFileManager.java
+++ JavarushMTProject/src/archiver/ZipFileManager.java
@@ -1,10 +1,13 @@
 package archiver;
 
+import archiver.exception.PathIsNotFoundException;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 public class ZipFileManager {
@@ -15,33 +18,36 @@ public class ZipFileManager {
     }
 
 
-    public void createZip(Path source) throws IOException {//пусть к файлу который мы будем архивировать
+    public void createZip(Path source) throws IOException, PathIsNotFoundException {//пусть к файлу который мы будем архивировать
         Path dir = zipFile.getParent();
         if(Files.notExists(dir)){
             Files.createDirectory(dir);
         }
-
-
-
         try(ZipOutputStream zos= new ZipOutputStream(Files.newOutputStream(zipFile));//открыли потока для записи в архив
             )//открыли поток для чтения из файла
         {if(Files.isRegularFile(source)){//если сорс - обычный файл, то просто его кладем
-            addNewZipEntry(zos,zipFile,source);
+            addNewZipEntry(zos,source.getParent(),source.getFileName());
+            //здесь - так как в методе принимается родительская директория и имя файла - нам нужно перевести
+            //сорс в такой вид - getParent и файлНейм
         } else if (
                 Files.isDirectory(source)
         ) {FileManager fm = new FileManager(source);
-
-
-
+            List<Path> fileNames = fm.getFileList();
+            for(Path p: fileNames){
+                addNewZipEntry(zos,source,p);
+            }
+        } else{
+            throw new PathIsNotFoundException();
         }
             zos.closeEntry();
         }
     }
     private void addNewZipEntry(ZipOutputStream zipOutputStream, Path filePath, Path filename) throws IOException{
-        ZipEntry ze = new ZipEntry(filename.toString());
-        try(InputStream is = Files.newInputStream(filename.resolve(filePath))){
-            zipOutputStream.putNextEntry(ze);
-            copyData(is,zipOutputStream);
+        ZipEntry ze = new ZipEntry(filename.toString());// создали зип Ентри - принимает строку на вход
+        try(InputStream is = Files.newInputStream(filePath.resolve(filename))){
+            //метод резолв - строит абсолютный путь из имени файла и его директории
+            zipOutputStream.putNextEntry(ze);// положили новый ентри с именем файла
+            copyData(is,zipOutputStream);//копируем данные в стрим
             zipOutputStream.closeEntry();
         }
     }
